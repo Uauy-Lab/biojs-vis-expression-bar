4842af8a2304d3dcd1ff871f745f620926f73e00
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-06-09 17:51:05 +0100

    Added prototypes

---
+++ snippets/exporession-bar-test.js
@@ -0,0 +1,12 @@
+
+//var bar = require("expressionbar");
+
+//console.log(expressionbar.hello("tada"));
+
+var container_div="bar_expression_viewer"
+
+var eb =  new expressionbar.ExoressionBar({
+	target: container_div
+}
+	
+);
