1d2c0f1cd7bdf359d03f32dde200921629268e3e
Ricardo H. Ramírez-Gonzalez <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-06-10 06:31:30 +0100

    The first sniper loads dthe data. Still we need to render it

--- snippets/exporession-bar-test.js
+++ snippets/exporession-bar-test.js
@@ -1,12 +1,13 @@
 
 //var bar = require("expressionbar");
 
-//console.log(expressionbar.hello("tada"));
+console.log(expressionbar.hello("tada"));
 
 var container_div="bar_expression_viewer"
 
 var eb =  new expressionbar.ExoressionBar({
-	target: container_div
+	target: container_div,
+	data: "data/test_data.json"
 }
 	
 );
