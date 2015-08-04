a7cca71a239c1435edbd6be824150ea49b113e79
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-08-04 19:28:31 +0100

    Group by factor works from the constructor

--- snippets/exporession-bar-test.js
+++ snippets/exporession-bar-test.js
@@ -5,7 +5,7 @@ var container_div="bar_expression_viewer";
 var eb =  new biovisexpressionbar.ExpressionBar({
 	target: container_div,
 	data: "data/test_data.json", 
-	groupBy: ["Age"]
+	groupBy: ['Age']
 }
 	
 );
