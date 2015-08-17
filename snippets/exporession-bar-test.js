425bd565c2e6f3fdf42b19e49f87cf589f477c6a
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-08-17 17:26:33 +0100

    Added scale bars

--- snippets/exporession-bar-test.js
+++ snippets/exporession-bar-test.js
@@ -5,7 +5,8 @@ var container_div="bar_expression_viewer";
 var eb =  new biovisexpressionbar.ExpressionBar({
 	target: container_div,
 	data: "data/test_data.json", 
-	groupBy: 'groups'
+	groupBy: 'groups', 
+	renderProperty: 'count'
 }
 	
 );
