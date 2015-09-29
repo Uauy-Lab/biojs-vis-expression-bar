5e2f1b8c4e24ebeb7ae73e49fdd5b9dc706e3f81
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-09-29 23:23:07 +0100

    Added buttons to set/unset factors\nFixed sorting of factors with spaces

--- snippets/exporession-bar-test.js
+++ snippets/exporession-bar-test.js
@@ -8,7 +8,7 @@ var eb =  new biovisexpressionbar.ExpressionBar({
 	//highlight: 'Traes_4AL_F99FCB25F.1', 
 	highlight: 'Traes_4AL_F9DCE24F4.1',
 	data: "data/realTestHom.js", 
-	groupBy: 'groups', 
+	groupBy: ["High level Stress-disease", "High level age","High level tissue","High level variety"], 
 	renderProperty: 'count', 
 	width: '1000',
 	fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif', 
