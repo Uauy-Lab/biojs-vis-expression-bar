56be4c4b972422274d5731d004718271abdc876d
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-10-10 12:14:22 +0100

    Now the plot works with incomplete data

---
+++ snippets/exporession-bar-empty.js
@@ -0,0 +1,22 @@
+var isNode = false;
+
+if (typeof module !== 'undefined' && module.exports) {
+            isNode = true;
+}
+
+if(isNode){
+	biovisexpressionbar = require("bio-vis-expression-bar");	
+}
+
+var container_div="bar_expression_viewer";
+
+var eb =  new biovisexpressionbar.ExpressionBar({
+	target: container_div,
+	data: "data/empty_stub.json", 
+	groupBy: ["High level Stress-disease", "High level age","High level tissue","High level variety"], 
+	renderProperty: 'count', 
+	width: '1000',
+	fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif', 
+	barHeight: 12
+}	
+);
