2f82be3a2dda785c459f4acf8a0968f28dab5b4c
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-12-25 09:12:30 -0600

    Added files to split the rendering even more

---
+++ snippets/heatmap-test.js
@@ -0,0 +1,24 @@
+var isNode = false;
+
+if (typeof module !== 'undefined' && module.exports) {
+            isNode = true;
+}
+
+if(isNode){
+	biovisexpressionbar = require("bio-vis-expression-bar");	
+}
+
+var container_div="bar_expression_viewer";
+
+var eb =  new biovisexpressionbar.ExpressionBar({
+	target: container_div,
+	highlight: 'Traes_4AL_F9DCE24F4.1',
+	data: "data/realTestHom.js", 
+	groupBy: ["High level Stress-disease", "High level age","High level tissue","High level variety"], 
+	renderProperty: 'count', 
+	width: '1000',
+	fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif', 
+	barHeight: 12,
+	plot:'HeatMap'
+}	
+);
