359c58a64e3069c5016a288d01f593c1d869875c
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-12-30 07:38:30 -0600

    Removed debug flags and improved example for the heatmap to display data by default

--- snippets/heatmap-test.js
+++ snippets/heatmap-test.js
@@ -12,9 +12,9 @@ var container_div="bar_expression_viewer";
 
 var eb =  new biovisexpressionbar.ExpressionBar({
 	target: container_div,
-	highlight: 'Traes_4AL_F9DCE24F4.1',
+	highlight: 'Traes_1DS_18F13A3DD.1',
 	data: "data/heatMapTest.js", 
-	groupBy: ["High level Stress-disease", "High level age","High level tissue","High level variety"], 
+	groupBy: ["High level stress-disease", "High level age","High level tissue","High level variety"], 
 	renderProperty: 'count', 
 	width: '1000',
 	fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif', 
