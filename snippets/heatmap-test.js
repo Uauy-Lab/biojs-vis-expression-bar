f176f05a6f1322b5a449ebe4511974f3cfd1bf44
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-12-30 07:19:09 -0600

    Added the heatmap test values

--- snippets/heatmap-test.js
+++ snippets/heatmap-test.js
@@ -13,7 +13,7 @@ var container_div="bar_expression_viewer";
 var eb =  new biovisexpressionbar.ExpressionBar({
 	target: container_div,
 	highlight: 'Traes_4AL_F9DCE24F4.1',
-	data: "data/realTestHom.js", 
+	data: "data/heatMapTest.js", 
 	groupBy: ["High level Stress-disease", "High level age","High level tissue","High level variety"], 
 	renderProperty: 'count', 
 	width: '1000',
