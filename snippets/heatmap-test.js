0e8e44b3920048f866c360e09258e80a003b2fe1
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2016-01-03 08:36:25 -0600

    Fixed the selector of values for cases where we don't have a default gene

--- snippets/heatmap-test.js
+++ snippets/heatmap-test.js
@@ -5,7 +5,6 @@ var container_div="bar_expression_viewer";
 
 var eb =  new biovisexpressionbar.ExpressionBar({
 	target: container_div,
-	highlight: 'Traes_1DS_18F13A3DD.1',
 	data: "data/heatMapTest.js", 
 	groupBy: ["High level stress-disease", "High level age","High level tissue","High level variety"], 
 	renderProperty: 'count', 
