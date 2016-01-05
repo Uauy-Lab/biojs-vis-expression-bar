4d68f94d4b3313ebdbb7931754dbfef48b487151
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2016-01-05 16:18:53 -0600

    Log bound to start with values over 1

--- snippets/heatmap-test.js
+++ snippets/heatmap-test.js
@@ -5,7 +5,7 @@ var container_div="bar_expression_viewer";
 
 var eb =  new biovisexpressionbar.ExpressionBar({
 	target: container_div,
-	data: "data/heatMapTest.js", 
+	data: "data/spike.js", 
 	groupBy: ["High level stress-disease", "High level age","High level tissue","High level variety"], 
 	renderProperty: 'count', 
 	width: '1000',
