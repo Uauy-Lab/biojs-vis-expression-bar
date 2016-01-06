c694446957acfdbcc2fe42660ff275b7a7e4f043
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2016-01-06 12:45:08 -0600

    Added vertical highlighting

--- snippets/heatmap-test.js
+++ snippets/heatmap-test.js
@@ -5,12 +5,13 @@ var container_div="bar_expression_viewer";
 
 var eb =  new biovisexpressionbar.ExpressionBar({
 	target: container_div,
-	data: "data/spike.js", 
+	data: "data/heatMapTest.js", 
 	groupBy: ["High level stress-disease", "High level age","High level tissue","High level variety"], 
 	renderProperty: 'count', 
 	width: '1000',
 	fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif', 
 	barHeight: 12,
+	headerOffset: 200,
 	plot:'HeatMap'
 }	
 );
