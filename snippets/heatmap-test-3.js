79b7527f052d78ef92bbf599883da6a02b29cbaf
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2016-02-25 20:47:42 +0000

    Added more snipets with heatmaps

---
+++ snippets/heatmap-test-3.js
@@ -0,0 +1,17 @@
+biovisexpressionbar = require("bio-vis-expression-bar");	
+
+
+var container_div="bar_expression_viewer";
+
+var eb =  new biovisexpressionbar.ExpressionBar({
+	target: container_div,
+	data: "data/3genes.json", 
+	groupBy: ["High level stress-disease", "High level age","High level tissue","High level variety"], 
+	renderProperty: 'count', 
+	width: '1000',
+	fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif', 
+	barHeight: 12,
+	headerOffset: 200,
+	plot:'HeatMap'
+}	
+);
