5356a94371211f5f887e50cf9e35bf2e5573681a
Bijan <bijan.ghasemiafshar@gmail.com>
2018-03-29 15:09:59 +0100

    + and - icons are fixed and the bar chart rescales according to the window size

--- snippets/exporession-bar-fix-homoeologues-sort.js
+++ snippets/exporession-bar-fix-homoeologues-sort.js
@@ -13,7 +13,20 @@ var eb =  new biovisexpressionbar.ExpressionBar({
 	fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif',
 	groupBy: ["High level stress-disease", "High level age","High level tissue","High level variety"], 
 	barHeight: 12,
-	width: 1000,	
+	width: window.innerWidth - 40,	
 	headerOffset:100	
 }	
 );
+
+// rescaling the bar chart after a 1.5 second delay of resizing the window ****************REMOVE THIS NOW!!!!!!!!!!!!!!!
+var resizeTimer;
+$(window).on('resize', function(e){      
+  clearTimeout(resizeTimer);  // Making sure that the reload doesn't happen if the window is resized within 1.5 seconds
+  resizeTimer = setTimeout(function(){
+    eb.resizeChart();
+  }, 1500);
+});
+
+$("button").click(function(){
+	$("h1").toggle("fast");
+});
