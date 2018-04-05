195e222f7a1cd6908ccaab6fa32545e4017ede71
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2018-04-05 13:34:23 +0100

    Fixed some merged conflicts

--- snippets/exporession-bar-test.js
+++ snippets/exporession-bar-test.js
@@ -8,14 +8,6 @@ var container_div="bar_expression_viewer";
 var eb =  new biovisexpressionbar.ExpressionBar({
 	target: container_div,
 	highlight: 'Traes_4AL_F9DCE24F4.1',
-<<<<<<< HEAD
-	data: window.location.href + "/../data/realTestHom.js", 
-	groupBy: ["High level stress-disease", "High level age","High level tissue","High level variety"], 
-	renderProperty: 'count', 
-	width: '1000',
-	fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif', 
-	barHeight: 12
-=======
 	data: window.location.href + "/../data/realTestHom.js", 	
 	renderProperty: 'tpm', 
 	fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif',
@@ -23,6 +15,5 @@ var eb =  new biovisexpressionbar.ExpressionBar({
 	barHeight: 12,
 	width: 1000,	
 	headerOffset:100	
->>>>>>> 040e55d6a21c66c5590d6a42c4d1dc7123499e6a
-}	
+}
 );
