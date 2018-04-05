c5ac15a976ef27588e916c37ad30bf19180544eb
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2018-04-05 13:28:08 +0100

    wirde merge

--- snippets/heatmap-test-3.js
+++ snippets/heatmap-test-3.js
@@ -8,10 +8,21 @@ var eb =  new biovisexpressionbar.ExpressionBar({
 	data: window.location.href + "/../data/3genes.json", 
 	groupBy: ["High level stress-disease", "High level age","High level tissue","High level variety"], 
 	renderProperty: 'count', 
-	width: '1000',
+	width: window.innerWidth - 60,
 	fontFamily:'Palatino Linotype, Book Antiqua, Palatino, serif', 
 	barHeight: 12,
 	headerOffset: 200,
 	plot:'HeatMap'
 }	
 );
+
+
+// rescaling the bar chart after a 1.5 second delay of resizing the window ****************REMOVE THIS NOW!!!!!!!!!!!!!!!
+var resizeTimer;
+$(window).on('resize', function(e){      
+  clearTimeout(resizeTimer);  // Making sure that the reload doesn't happen if the window is resized within 1.5 seconds
+  resizeTimer = setTimeout(function(){
+  	console.log("We are resizing now and want to call resize Chart");
+    eb.resizeChart();
+  }, 1500);
+});
